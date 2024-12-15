8a06b0a2c241a0841ebaf61a780e6d69d61c0abd
Betta <dev_fun_lorr@proton.me>
2024-12-15 23:59:25 +0100

    Ajout des 1eres pierres des routes.

--- Cyna-Back/app.ts
+++ Cyna-Back/app.ts
@@ -1,8 +1,45 @@
-import Koa from 'koa';
-import koaBody from 'koa-body';
+// import Koa from 'koa';
+// import koaBody from 'koa-body';
 // import router from 'koa-router'
+// const app = new Koa();
+//
+// app.use(koaBody());
+//
+// export default app;
+
+
+import Koa from 'koa';
+// import bodyParser from 'koa-bodyparser';
+import Router from 'koa-router';
+
 const app = new Koa();
+const router = new Router();
+// app.use(bodyParser());
 
-app.use(koaBody());
+router
+    .get('/', (ctx, next) => {
+        ctx.body = 'Hello World!';
+    })
+    .post('/',(ctx, next) => {
+      console.log("Test");
+      ctx.body = 'Hello World!';
 
-export default app;
+    })
+    .post('/users', (ctx, next) => {
+        // handle your post request here
+        // ctx.body = ctx.request.body;
+        console.log("Hi")
+    })
+    .put('/users/:id', (ctx, next) => {
+        // ...
+    })
+    .del('/users/:id', (ctx, next) => {
+        // ...
+    })
+    .all('/users/:id', (ctx, next) => {
+        // ...
+    });
+
+app.use(router.routes())
+app.use(router.allowedMethods());
+export default app
