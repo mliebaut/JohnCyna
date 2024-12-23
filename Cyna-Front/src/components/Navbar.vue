87de3fbf85982369dc6bb742b46887ae8be5aa25
Thanh680 <nguyenthanhdanh680@gmail.com>
2024-12-23 22:48:33 +0100

    Update section categorie

--- Cyna-Front/src/components/Navbar.vue
+++ Cyna-Front/src/components/Navbar.vue
@@ -1,28 +1,61 @@
 <template>
 <nav class="navbar bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <img src="/site-logo.png" width="120" alt="Cyna">
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <div class="container">
+    <img src="/site-logo.png" width="180" alt="Cyna">
+    <div>
+      <a class="ms-4" aria-current="page" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" viewBox="0 0 576 512"><path fill="#7200ff" d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg></a>
+      <a class="ms-4" aria-current="page" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" viewBox="0 0 512 512"><path fill="#7200ff" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></a>
+      <button class="navbar-toggler ms-4" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <img src="/site-logo.png" width="120" alt="Cyna">
+        <img src="/site-logo.png" width="140" alt="Cyna">
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+      <div class="offcanvas-body d-contents">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 fs-5">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="#">Accueil</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Catégorie</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Panier</a>
+          </li>
+          <li class="nav-item">
+            <button class="btn btn-primary m-2 w-75 fs-5">Se connecter</button>
+          </li>
+          <li class="nav-item">
+            <button class="btn btn-primary m-2 w-75 fs-5">Créer un compte</button>
           </li>
         </ul>
-        <form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+
+        <div class="navbar-nav">
+
+        <div class="navbar-nav d-flex align-items-center justify-content-center link-body-emphasis text-decoration-none">
+            <ul class="nav-item list-unstyled d-flex">
+            <li class="ms-2"><a class="text-body-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+            </svg></a></li>
+            <li class="ms-2"><a class="text-body-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+            </svg></a></li>
+            <li class="ms-2"><a class="text-body-secondary" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+            </svg></a></li>
+          </ul>
+        </div>
+
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a href="#" class="nav-link text-body-secondary">Mentions légales</a></li>
+          <li class="nav-item"><a href="#" class="nav-link text-body-secondary">CGU</a></li>
+          <li class="nav-item"><a href="#" class="nav-link text-body-secondary">Contact</a></li>
+          <li class="nav-item"><p class="text-body-secondary mt-2">&copy; 2024 Company, Inc</p></li>
+        </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -36,6 +69,21 @@
   </script>
   
   <style>
-
+    .btn-primary{
+      --bs-btn-color: #fff;
+      --bs-btn-bg: #7200ff;
+      --bs-btn-border-color: #7200ff;
+      --bs-btn-hov7200ffer-color: #fff;
+      --bs-btn-hover-bg: #7200ff;
+      --bs-btn-hover-border-color: #4d00aa;
+      --bs-btn-focus-shadow-rgb: 49, 132, 253;
+      --bs-btn-active-color: #fff;
+      --bs-btn-active-bg: #4d00aa;
+      --bs-btn-active-border-color: #4d00aa;
+      --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+      --bs-btn-disabled-color: #fff;
+      --bs-btn-disabled-bg: #7c10ff;
+      --bs-btn-disabled-border-color: #7708ff;
+    }
   </style>
   
--- Cyna-Front/src/views/Home.vue
+++ Cyna-Front/src/views/Home.vue
@@ -2,12 +2,12 @@
     <div class="mt-5">
       <!-- Carousel -->
       <div id="carouselExampleDark" class="carousel carousel-dark slide pt-5">
-        <div class="carousel-indicators">
+        <div class="carousel-indicators ">
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
-      <div class="carousel-inner h-100">
+      <div class="carousel-inner container h-100">
         <div class="carousel-item row active h-100">
           <div class="banner h-100">
             <div class="container h-100">
@@ -15,7 +15,7 @@
                 <div class="col-sm-5 text-start">
                   <h1>Cyna protège votre entreprise contre les cyberattaques</h1>
                   <p>Pure player en cybersécurité pour les PME & ETI. </p>
-                  <button class="btn btn-primary">Learn More</button>
+                  <button class="btn btn-primary fs-3 px-4">Learn More</button>
                 </div>
                 <div class="col-sm-7">
                   <img class="img-fluid" src="https://via.placeholder.com/800x400?text=Product%201" alt="">
@@ -31,7 +31,7 @@
                 <div class="col-sm-5 text-start">
                   <h1>Cyna protège votre entreprise contre les cyberattaques</h1>
                   <p>Pure player en cybersécurité pour les PME & ETI. </p>
-                  <button class="btn btn-primary">Learn More</button>
+                  <button class="btn btn-primary fs-3 px-4">Learn More</button>
                 </div>
                 <div class="col-sm-7">
                   <img class="img-fluid" src="https://via.placeholder.com/800x400?text=Product%202" alt="">
@@ -47,7 +47,7 @@
                 <div class="col-sm-5 text-start">
                   <h1>Cyna protège votre entreprise contre les cyberattaques</h1>
                   <p>Pure player en cybersécurité pour les PME & ETI. </p>
-                  <button class="btn btn-primary">Learn More</button>
+                  <button class="btn btn-primary fs-3 px-4">Learn More</button>
                 </div>
                 <div class="col-sm-7">
                   <img class="img-fluid" src="https://via.placeholder.com/800x400?text=Product%203" alt="">
@@ -57,62 +57,45 @@
           </div>
         </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <button class="carousel-control-prev w-auto" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span class="visually-hidden">Précédant</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <button class="carousel-control-next w-auto" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span class="visually-hidden">Suivant</span>
         </button>
       </div>
 
+      <!-- Banderole -->
       <div class="banderole">
         <h4 class="container">Votre sécurité est notre métier,
           construisez vos projets en toute sérénité.</h4>
       </div>
   
       <!-- Categories -->
-      <h2 class="mb-4">Categories</h2>
-      <div class="row container gap-5 m-auto">
-        <div class="col">
-          <div class="card text-center">
-            <img src="https://via.placeholder.com/100x100?text=Category%201" class="card-img-top" alt="Category 1">
-            <div class="card-body">
-              <h5 class="card-title">Categorie 1</h5>
-              <p class="card-text">Description de la catégorie</p>
-              <a href="#" class="btn btn-primary">Voir catégorie</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card text-center">
-            <img src="https://via.placeholder.com/100x100?text=Category%202" class="card-img-top" alt="Category 2">
-            <div class="card-body">
-              <h5 class="card-title">Categorie 2</h5>
-              <p class="card-text">Description de la catégorie</p>
-              <a href="#" class="btn btn-primary">Voir catégorie</a>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card text-center">
-            <img src="https://via.placeholder.com/100x100?text=Category%3F" class="card-img-top" alt="Category 3">
-            <div class="card-body">
-              <h5 class="card-title">Categorie 3</h5>
-              <p class="card-text">Description de la catégorie</p>
-              <a href="#" class="btn btn-primary">Voir catégorie</a>
+      <h2 class="mb-4">Catégories</h2>
+      <div class="category row justify-content-md-center container m-auto row-gap-5">
+        <div class="col-xs-1 col-md-4" v-for="category in categories" :key="category.title">
+          <div class="card text-center m-auto" :style="{backgroundImage:`url(${category.image})`}">
+            <div class="card-body d-flex flex-column justify-content-center align-items-center ">
+              <h3 class="card-title">{{ category.title }}</h3>
+                <p class="card-text fs-5 fw-medium">{{ category.description }}</p>
+                <a href="#" class="btn btn-primary">Voir catégorie</a>
             </div>
           </div>
         </div>
       </div>
   
-      <!-- Top Products -->
-      <h2 class="mt-5 mb-4">Top Products</h2>
-      <div class="row container gap-5 m-auto">
-        <div class="col" v-for="(product, index) in topProducts" :key="index">
-          <div class="card text-center">
-            <img src="https://via.placeholder.com/100x100" class="card-img-top" alt="Product {{ index + 1 }}">
+      <!-- Meilleurs Produits -->
+      <h2 class="mt-5 mb-4">Les Top Produits du moment</h2>
+      <div class="row container justify-content-md-center row-gap-5 m-auto">
+        <div class="col-xs-1 col-md-4" v-for="(product, rank) in topProducts" :key="rank">
+          <svg v-if="product.rank === 1" width="100" height="100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#FFD43B" d="M4.1 38.2C1.4 34.2 0 29.4 0 24.6C0 11 11 0 24.6 0L133.9 0c11.2 0 21.7 5.9 27.4 15.5l68.5 114.1c-48.2 6.1-91.3 28.6-123.4 61.9L4.1 38.2zm503.7 0L405.6 191.5c-32.1-33.3-75.2-55.8-123.4-61.9L350.7 15.5C356.5 5.9 366.9 0 378.1 0L487.4 0C501 0 512 11 512 24.6c0 4.8-1.4 9.6-4.1 13.6zM80 336a176 176 0 1 1 352 0A176 176 0 1 1 80 336zm184.4-94.9c-3.4-7-13.3-7-16.8 0l-22.4 45.4c-1.4 2.8-4 4.7-7 5.1L168 298.9c-7.7 1.1-10.7 10.5-5.2 16l36.3 35.4c2.2 2.2 3.2 5.2 2.7 8.3l-8.6 49.9c-1.3 7.6 6.7 13.5 13.6 9.9l44.8-23.6c2.7-1.4 6-1.4 8.7 0l44.8 23.6c6.9 3.6 14.9-2.2 13.6-9.9l-8.6-49.9c-.5-3 .5-6.1 2.7-8.3l36.3-35.4c5.6-5.4 2.5-14.8-5.2-16l-50.1-7.3c-3-.4-5.7-2.4-7-5.1l-22.4-45.4z"/></svg>
+          <svg v-else-if="product.rank === 2" width="100" height="100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#dedede" d="M4.1 38.2C1.4 34.2 0 29.4 0 24.6C0 11 11 0 24.6 0L133.9 0c11.2 0 21.7 5.9 27.4 15.5l68.5 114.1c-48.2 6.1-91.3 28.6-123.4 61.9L4.1 38.2zm503.7 0L405.6 191.5c-32.1-33.3-75.2-55.8-123.4-61.9L350.7 15.5C356.5 5.9 366.9 0 378.1 0L487.4 0C501 0 512 11 512 24.6c0 4.8-1.4 9.6-4.1 13.6zM80 336a176 176 0 1 1 352 0A176 176 0 1 1 80 336zm184.4-94.9c-3.4-7-13.3-7-16.8 0l-22.4 45.4c-1.4 2.8-4 4.7-7 5.1L168 298.9c-7.7 1.1-10.7 10.5-5.2 16l36.3 35.4c2.2 2.2 3.2 5.2 2.7 8.3l-8.6 49.9c-1.3 7.6 6.7 13.5 13.6 9.9l44.8-23.6c2.7-1.4 6-1.4 8.7 0l44.8 23.6c6.9 3.6 14.9-2.2 13.6-9.9l-8.6-49.9c-.5-3 .5-6.1 2.7-8.3l36.3-35.4c5.6-5.4 2.5-14.8-5.2-16l-50.1-7.3c-3-.4-5.7-2.4-7-5.1l-22.4-45.4z"/></svg>
+          <svg v-else-if="product.rank === 3" width="100" height="100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#a65c07" d="M4.1 38.2C1.4 34.2 0 29.4 0 24.6C0 11 11 0 24.6 0L133.9 0c11.2 0 21.7 5.9 27.4 15.5l68.5 114.1c-48.2 6.1-91.3 28.6-123.4 61.9L4.1 38.2zm503.7 0L405.6 191.5c-32.1-33.3-75.2-55.8-123.4-61.9L350.7 15.5C356.5 5.9 366.9 0 378.1 0L487.4 0C501 0 512 11 512 24.6c0 4.8-1.4 9.6-4.1 13.6zM80 336a176 176 0 1 1 352 0A176 176 0 1 1 80 336zm184.4-94.9c-3.4-7-13.3-7-16.8 0l-22.4 45.4c-1.4 2.8-4 4.7-7 5.1L168 298.9c-7.7 1.1-10.7 10.5-5.2 16l36.3 35.4c2.2 2.2 3.2 5.2 2.7 8.3l-8.6 49.9c-1.3 7.6 6.7 13.5 13.6 9.9l44.8-23.6c2.7-1.4 6-1.4 8.7 0l44.8 23.6c6.9 3.6 14.9-2.2 13.6-9.9l-8.6-49.9c-.5-3 .5-6.1 2.7-8.3l36.3-35.4c5.6-5.4 2.5-14.8-5.2-16l-50.1-7.3c-3-.4-5.7-2.4-7-5.1l-22.4-45.4z"/></svg>
+          <div class="card text-center m-auto mt-3">
+            <img :src="(`${product.image}`)" height="300" class="card-img-top" alt="Product {{ index + 1 }}">
             <div class="card-body">
               <h5 class="card-title">{{ product.name }}</h5>
               <p class="card-text">Prix: {{ product.price }} €</p>
@@ -123,15 +106,36 @@
       </div>
     </div>
   </template>
-  
   <script>
   export default {
     data() {
       return {
         topProducts: [
-          { name: 'Product 1', price: 29.99 },
-          { name: 'Product 2', price: 49.99 },
-          { name: 'Product 3', price: 79.99 },
+          { name: 'Produit 1', price: 29.99, rank: 1, image: '/product1.jpg'},
+          { name: 'Produit 2', price: 49.99, rank: 2, image: '/product2.jpg'},
+          { name: 'Produit 3', price: 79.99, rank: 3, image: '/product3.jpg'},
+        ],
+        categories: [
+          { 
+            title: 'Prévention', 
+            description: "Mettre en place des mesures et des bonnes pratiques pour éviter les cyberattaques. Cela inclut la formation, l'évaluation des risques et la mise en place de politiques de sécurité adaptées pour anticiper les menaces.",
+            image: '/Categorie1.jpg' 
+          },
+          { 
+            title: 'Protection', 
+            description: "Sécuriser vos systèmes et données contre les cyberattaques grâce à des solutions avancées, comme les pare-feu, les antivirus, et la gestion des accès, pour garantir une défense solide et continue.",
+            image: '/Categorie2.jpg'
+           },
+          { 
+            title: 'Réponse', 
+            description: "Réagir rapidement et efficacement en cas d'incident de cybersécurité, avec des processus de gestion de crise, de récupération des données et de coordination avec les autorités pour minimiser l'impact des attaques.",
+            image: '/Categorie3.jpg'
+          },
+          { 
+            title: 'Anticipation', 
+            description: "Surveiller, analyser et prédire les évolutions des menaces pour ajuster en permanence vos défenses. Grâce à une veille proactive et des technologies avancées, nous identifions les risques potentiels avant qu'ils ne deviennent des menaces réelles.",
+            image: '/Categorie2.jpg' 
+          },
         ],
       };
     },
@@ -142,7 +146,7 @@
 
     #carouselExampleDark{
       height: 48em;
-      background-image: url(../../public/background-hero.png);
+      background-image: url(/background-hero.png);
       background-size: cover;
       background-repeat: no-repeat;
     }
@@ -152,11 +156,41 @@
     }
 
     .carousel-item h1{
-      font-size: 60px;
+      font-size: 42px;
     }
 
     .carousel-item p{
       font-size: 30px;
+    }
+
+    .category .card{
+      height: 400px;
+      color: white;
+      text-shadow: #000 0px 0px 1px,   #000 0px 0px 1px,   #000 0px 0px 1px;
+    }
+
+    .category .card-content {
+      transition-duration: 0.5s;
+      transition-timing-function: ease-in-out;  
+    }
+        
+    .category .card-content:hover {
+      display: block;
+    }
+
+    .category .card p{
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      max-height: 100px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .card{
+      max-width: 400px;
+      background-size: cover;
+      background-repeat: no-repeat;
     }
 
     .btn-primary{
@@ -176,16 +210,31 @@
       --bs-btn-disabled-border-color: #7708ff;
     }
     .banderole{
-      height: 30em;
+      height: 20em;
       display: flex;
       justify-content: center; 
       align-items: center;
     }
 
     .banderole h4{
-      font-size: 50px;
+      font-size: 30px;
       justify-content: center; 
       align-items: center;
       display: flex;
+    }
+
+    @media (min-width: 768px) {
+      .carousel-item h1{
+        font-size: 60px;
+      }
+      .banderole{
+      height: 30em;
+      display: flex;
+      justify-content: center; 
+      align-items: center;
+      }
+      .banderole h4{
+        font-size: 50px;
+      }
     }
   </style>
