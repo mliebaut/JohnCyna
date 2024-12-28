5523dd48a1ef0830652e0c62284a2dc3251184b0
Betta <dev_fun_lorr@proton.me>
2024-12-28 16:50:11 +0100

    CRUD Basique pour User. WIP.

--- Cyna-Back/routes/userRoutes.ts
+++ Cyna-Back/routes/userRoutes.ts
@@ -15,8 +15,8 @@ userRouter
         console.log(e)
     }
     })
-    .post('/user_create', async (ctx, next) => {
-        console.log("/user_create");
+    .post('/user/create', async (ctx, next) => {
+        console.log("/user/create");
         try {
             const receivedData = ctx.request.body;
             console.log(`Request Body: ${JSON.stringify(ctx.request.body)}`)
@@ -33,6 +33,43 @@ userRouter
         } catch (e) {
             console.log(e)
         }
-    });
+    })
+    // - TODO: Mettre en place une mise à jour selective. Genre, tu envoie seulement le nom et prenom, ca change que ça.
+    .post('user/update', async (ctx, next) => {
+        console.log("/user/delete");
+        try {
+            const receivedData = ctx.request.body;
+            console.log(`Request Body: ${JSON.stringify(ctx.request.body)}`)
+            console.log(receivedData);
+            const userToUpdate: any = await prisma.user.update({
+                where: {
+                    id: receivedData.userId
+                },
+                data: {
+                //     METTRE LES ARGUMENTS ICI
+                }
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    })
+    .del('/user/delete', async (ctx, next) => {
+        console.log("/user/delete");
+        try {
+            const receivedData = ctx.request.body;
+            console.log(`Request Body: ${JSON.stringify(ctx.request.body)}`)
+            console.log(receivedData);
+            const userToDelete: any = await prisma.user.delete({
+                where: {
+                    id: receivedData.userId
+                }
+            })
+            console.log(userToDelete)
+            ctx.body = userToDelete
+        } catch (e) {
+            console.log(e)
+        }
+    })
+;
 
 export default userRouter
