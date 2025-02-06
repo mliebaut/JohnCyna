58c8b27dd304944d4e578bffd78e565849eae254
Thom <forbiddenbetta@gmail.com>
2025-02-06 15:15:16 +0100

    Travail page debug

--- Cyna-Front/src/views/DebugView.vue
+++ Cyna-Front/src/views/DebugView.vue
@@ -1,31 +1,37 @@
-
 <template>
   <div>
     <h3>Plage de debug</h3>
     <hr>
-    <div>
-      Debug du ping
-      <button @click="ping_server()"></button>
-    </div>
-    <div>
-      ID : {{ userID }}
-      <input v-model="userID">
+    <div class="container-fluid">
+      <div>
+        Debug du ping
+        <button @click="ping_server()"></button>
+      </div>
+      <div>
+        Find all Users
+        <div></div>
+      </div>
+      <div>
+        Find all products
+        <div></div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          import {ref} from "vue";
+import {ref} from "vue";
 import Serv_Url from "../main.ts";
 
 const userID = ref("")
 
-async function ping_server(){
+async function ping_server() {
   const response = await fetch(Serv_Url, {
     method: "POST",
-    body: JSON.stringify({ username: "example" }),
+    body: JSON.stringify({username: "example"}),
     // headers: myHeaders,
-    mode:"no-cors"
+    mode: "no-cors"
   });
   console.log(response);
 }
