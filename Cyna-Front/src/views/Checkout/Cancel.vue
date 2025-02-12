a7dd62ba202acca274332bbaf51ba5760e8a08fb
mliebaut <melusine.liebaut@gmail.com>
2025-02-12 17:02:39 +0100

    Fix margin on cancel vue

--- Cyna-Front/src/views/Checkout/Cancel.vue
+++ Cyna-Front/src/views/Checkout/Cancel.vue
@@ -1,8 +1,8 @@
 // Cancel.vue
 
 <template>
-    <div>
-        <h1 class="mt-5">Paiement annulé 😢</h1>
+    <div class="mt-5">
+        <h1>Paiement annulé 😢</h1>
         <p>Une erreur est survenue ou vous avez annulé le paiement.</p>
         <router-link to="/">Réessayer</router-link>
     </div>
