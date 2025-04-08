cf2424e56bd22e177cf6d3609178da953a7910f8
betta <forbiddenbetta@gmail.com>
2025-04-08 21:14:09 +0200

    Formattage routeur

--- Cyna-Front/src/router/index.ts
+++ Cyna-Front/src/router/index.ts
@@ -1,12 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 // import AboutView from './AboutView.vue'
 //import HomeView from '../components/HelloWorld.vue'
 import HomeView from "../views/Home.vue";
 import CategoryViewView from '../views/CategoryView.vue';
-import Checkout from "@/components/Checkout.vue";
-import Success from "@/components/Checkout/Success.vue";
-import Cancel from "@/components/Checkout/Cancel.vue";
 import AjoutPanierView from '../views/AjoutPanierView.vue';
 
 import HomeView1 from '../views/HomeView1.vue';
@@ -19,17 +16,17 @@ const routes = [
         name: 'ajout-panier',
         component: AjoutPanierView
     },
-    
 
-      {
-        path: '/',
-        name: 'Home',
-        component: HomeView1
-      },
+
     {
         path: '/',
         name: 'Home',
-        component: HomeView 
+        component: HomeView1
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView
     },
     {
         path: '/category/:title',
@@ -39,12 +36,12 @@ const routes = [
     {
         path: '/about',
         name: 'About',
-        component: () => import("../views/AboutView.vue") 
+        component: () => import("../views/AboutView.vue")
     },
     {
         path: '/contactOld',
         name: 'ContactOld',
-        component: () => import("../views/ContactOldView.vue") 
+        component: () => import("../views/ContactOldView.vue")
     },
     {
         path: '/contact',
@@ -57,25 +54,25 @@ const routes = [
         component: () => import("../views/MentionsLegalesView.vue")
     },
     {
-        path:'/inscription',
-        name:'Inscription',
+        path: '/inscription',
+        name: 'Inscription',
         component: () => import("../views/InscriptionView.vue")
     },
     {
-        path:'/connexion',
-        name:'Connexion',
+        path: '/connexion',
+        name: 'Connexion',
         component: () => import("../views/ConnexionView.vue")
     },
 
     {
-        path:'/produit',
-        name:'produit',
+        path: '/produit',
+        name: 'produit',
         component: () => import("../views/ProduitView.vue")
     },
     {
         path: '/panier',
         name: 'Panier',
-        component: () => import("../views/PanierView.vue") 
+        component: () => import("../views/PanierView.vue")
     },
     {
         path: '/debug',
@@ -87,37 +84,40 @@ const routes = [
         name: 'CGU',
         component: () => import("../views/CguView.vue")
     },
-    {   path: "/checkout",
-        name: 'checkout', 
-        component: () => import("../views/Checkout.vue") 
+    {
+        path: "/checkout",
+        name: 'checkout',
+        component: () => import("../views/Checkout.vue")
     },
-    {   path: "/success",
-        name: 'success', 
-        component: () => import("../views/Checkout/Success.vue") 
+    {
+        path: "/success",
+        name: 'success',
+        component: () => import("../views/Checkout/Success.vue")
     },
-    {   path: "/cancel", 
+    {
+        path: "/cancel",
         name: 'cancel',
-        component: () => import("../views/Checkout/Cancel.vue") 
+        component: () => import("../views/Checkout/Cancel.vue")
     },
     {
         path: "/historique",
         name: "Historique",
-        component: () => import("../views/OrderHistory.vue") 
+        component: () => import("../views/OrderHistory.vue")
     },
     {
         path: '/account',
         name: 'UserAccount',
         component: () => import('../views/UserAccount.vue'),
-        
-      }
+
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
     scrollBehavior() {
-        return { top: 0 }
-      },
+        return {top: 0}
+    },
 })
 
 export default router
