2004430d052b9fec28754487c7f73d53b24ed88c
Thanh <thanh-danh.nguyen@mewo-campus.fr>
2025-05-07 17:01:47 +0200

    Connexion corrigé sauf bouton confirmation mail

--- Cyna-Front/src/stores/user.js
+++ Cyna-Front/src/stores/user.js
@@ -56,6 +56,19 @@ export const useUserStore = defineStore("user", {
           document.querySelector('.msg-alert').style.display = 'block';
         } 
         else{
+          await this.login(email, password)
+        }
+      }
+      } catch (e) {
+        console.log(e);
+    }
+    },
+    async login(email, password) {
+      try {
+        const res = await fetch(Serv_Url + '/user/login', {
+          method: "POST",
+          body: JSON.stringify({ email, password }),
+        });
           const user = await res.json();
           this.user = user;
           if (this.returnUrl != null){
@@ -64,8 +77,6 @@ export const useUserStore = defineStore("user", {
           }else {
             window.location.href = "/";
           }
-        }
-      }
       } catch (e) {
         console.log(e);
     }
