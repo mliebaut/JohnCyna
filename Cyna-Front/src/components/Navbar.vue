93503b53ae3e6159f8b68e78adb19ea2a3352056
Betta <forbiddenbetta@gmail.com>
2025-05-27 13:29:27 +0200

    Ajout lien vers compte dans Navbar.vue

--- Cyna-Front/src/components/Navbar.vue
+++ Cyna-Front/src/components/Navbar.vue
@@ -56,6 +56,12 @@
                 </div>
               </ul>
             </li>
+            <li>
+              <router-link v-if="userStore.user && userStore.user.role !== USER" class="btn btn-info m-2 w-75 fs-5" to="/backoffice">Backoffice</router-link>
+            </li>
+            <li>
+              <router-link v-if="userStore.user" class="btn btn-warning m-2 w-75 fs-5" to="/account">Mon Compte</router-link>
+            </li>
             <li v-if="userStore.user" class="nav-item mt-5">
               <p class="text-body-secondary">Bonjour, {{ userStore.user.lastName }}</p>
               <a @click="userStore.logout()" class="nav-link" href="#">Se déconnecter</a>
@@ -70,9 +76,7 @@
               </li>
             </div>
           </ul>
-          <div>
-            <router-link v-if="userStore.user && userStore.user.role !== USER" class="btn btn-info m-2 w-75 fs-5" to="/backoffice">Backoffice</router-link>
-          </div>
+
 
           <div class="navbar-nav">
 
@@ -95,7 +99,7 @@
                         d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                   </svg>
                 </a></li>
-                <li class="ms-2"><a class="text-body-secondary" href="#">
+                <li class="ms-2"><a class="text-body-secondary" href="/debug">
                   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
                        class="bi bi-instagram" viewBox="0 0 16 16">
                     <path
